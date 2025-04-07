734dc3b38d24118449b5bf0b8a8b7287266da150
Gouranshvaishnavji <gouransh.v@kalvium.community>
2025-04-07 10:46:23 +0530

    this the only commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import WeatherLayout from "./components/weatherlayout/Weatherlayout";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherLayout />
+    </div>
+  );
+}
+
+export default App;
