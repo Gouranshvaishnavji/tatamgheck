d77be0b2925dc81b77cf113574ae97518b3f86e7
Gouranshvaishnavji <gouransh.v@kalvium.community>
2025-04-07 11:00:09 +0530

    jner

--- src/js/containers/Home/Home.js
+++
@@ -1,9 +0,0 @@
-import WeatherLayout from "../components/WeatherLayout"; // or correct path if it's in a different folder
-
-const Home = () => (
-  <div>
-    <WeatherLayout />
-  </div>
-);
-
-export default Home;
